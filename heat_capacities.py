09dd246f090e7e755f3963616350aa8d2f4f1dad
sofie-rk <sofierk@stud.ntnu.no>
2021-02-12 11:25:54 +0100

    initial commit

---
+++ heat_capacities.py
@@ -0,0 +1,66 @@
+def cp(A, B, C, D, E):
+    cp= A+B*t+C*t**2+D*t**3+E/t**2
+    return cp
+
+# constants for gaseous water [J/mol*K]
+A_w_g=30.09200
+B_w_g=6.832514
+C_w_g=6.793435
+D_w_g=-2.534480	
+E_w_g=0.082139
+
+#constants for liquid water [J/mol*K]
+A_w_l=-203.6060
+B_w_l=1523.290
+C_w_l=-3196.413
+D_w_l=2474.455
+E_w_l=3.855326
+
+#constants for N2(g) [J/mol*K]
+A_N2=28.98641
+B_N2=1.853978
+C_N2=-9.647459
+D_N2=16.63537
+E_N2=0.000117
+
+#constants for CO2(g) [J/mol*K]
+A_CO2=24.99735
+B_CO2=55.18696
+C_CO2=-33.69137
+D_CO2=7.948387
+E_CO2=-0.136638
+
+#constants for SO2(g) [J/mol*K]
+A_SO2=21.43049
+B_SO2=74.35094
+C_SO2=-57.75217
+D_SO2=16.35534
+E_SO2=0.086731
+
+#constants for NO(g) [J/mol*K]
+A_NO=23.83491
+B_NO=12.58878
+C_NO=-1.139011
+D_NO=-1.497459
+E_NO=0.214194
+
+#constants for NO(g) [J/mol*K]
+A_NO=23.83491
+B_NO=12.58878
+C_NO=-1.139011
+D_NO=-1.497459
+E_NO=0.214194
+
+#constants for Cl_2(g) [J/mol*K]
+A_Cl2=33.05060
+B_Cl2=12.22940
+C_Cl2=-12.06510
+D_Cl2=4.385330
+E_Cl2=-0.159494
+
+#constants for O_2(g) [J/mol*K]
+A_O2=31.32234
+B_O2=-20.23531
+C_O2=57.86644
+D_O2=-36.50624
+E_O2=-0.007374
