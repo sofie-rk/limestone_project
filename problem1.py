1b92f210e2714d159ae6ffea57d5dff8215bdc2b
sofie-rk <sofierk@stud.ntnu.no>
2021-03-04 19:35:44 +0100

    problem1.py finished

--- problem1.py
+++ problem1.py
@@ -4,13 +4,14 @@ from values import deltaH_rxn, deltaH_evap, LHV_coal
 from values import x_CO2_in, x_N2_in, x_SO2_in, x_NO_in, x_Cl2_in, x_H2O_in, x_O2_in
 from heat_capacities import *
 
-
+# sympy.integrate is used for integrating heat capacity
 from sympy.abc import x
 from sympy import integrate
 
 
 ### HEAT REQUIRED IN THE REACTION ###
 Q_reaction = deltaH_rxn * n_ls_dry_pure
+
 
 ### HEAT REQUIRED TO EVAPORATE WATER IN THE LIMESTONE ###
 
@@ -20,17 +21,12 @@ Q_heat_w_liq = integrate(cp(A_w_l, B_w_l, C_w_l, D_w_l, E_w_l, x), (x, T_fg_in, 
 # Heat water gas from 100C to 300C (flue gas outlet temperature)
 Q_heat_w_gas = integrate(cp(A_w_g, B_w_g, C_w_g, D_w_g, E_w_g, x), (x, T_H2O_boil, T_fg_out))
 
-# print("Liq: ", Q_heat_w_liq)
-# print("Evap: ", deltaH_evap)
-# print("Gas: ", Q_heat_w_gas)
-
 Q_evaporation = (Q_heat_w_liq + deltaH_evap + Q_heat_w_gas) * n_H2O_ls
 
 
 ### HEAT REQUIRED TO HEAT THE FLUE GAS TO 300C ###
 
 # CO2 produced in reaction must be heated from 25C til 300C
-
 Q_CO2 = n_CO2_gen * integrate(cp(A_CO2, B_CO2, C_CO2, D_CO2, E_CO2, x), (x, T_fg_in, T_fg_out))
 
 # Need to heat the flue gas from 25C to 300C
@@ -48,24 +44,15 @@ Q_fg = integrate(   x_CO2_in * cp(A_CO2, B_CO2, C_CO2, D_CO2, E_CO2, x) + \
 ### HEAT LOSS TO THE ENVIRONMENT ###
 
 Q_heat_loss = 0
-X = 0
+X = 0               # percentage heat loss
 
 
 
 ### ENERGY BALANCE ###
 
-# LHV * mass_coal = Q_evaporation + Q_reaction + Q_heat_loss + Q_CO2 + Q_fl*n_fg*mass_coal
+# LHV * mass_coal = Q_evaporation + Q_reaction + (Q_heat_loss) + Q_CO2 + Q_fl*n_fg*mass_coal
 
 mass_coal = (Q_evaporation + Q_reaction + Q_CO2 ) / (LHV_coal - Q_fg*n_fg - X*LHV_coal)
 
-#print("Q flue gas: " , Q_fg*n_fg)
-#print("Q reaction + CO2: ", Q_reaction + Q_CO2)
-
-#print("Q evaporation: ", Q_evaporation)
 
 print("MASS COAL: ", round(mass_coal/1000), " tonnes")
-
-### FURTHER CALCULATIONS ###
-
-#print("N_fg_tot IN", n_fg*mass_coal)
-#print("N_fg_tot OUT", n_fg*mass_coal + n_CO2_gen + n_H2O_ls)
