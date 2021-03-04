8b59b0ced8cd1432a248216594fb100ca39fe0ba
sofie-rk <sofierk@stud.ntnu.no>
2021-03-04 19:59:30 +0100

    finished length_of_the_kiln.py

--- length_of_the_kiln.py
+++ length_of_the_kiln.py
@@ -1,3 +1,7 @@
+# Calculates the total residence time and the length of the kiln
+# Scrip problem4_drying.py and problem4_calcination.py runs and provides the
+# residence time for calcination and drying
+
 from problem4_calcination import tau3_c
 from problem4_drying import tau3_d
 
@@ -6,12 +10,6 @@ from problem1 import mass_coal
 from values import N, S, d_kiln
 from values import seconds_in_a_minute
 
-
-# tau3_c = 26100
-# tau3_d = 2730
-
-# tau3_c = 44500
-# tau3_d = 4650
 
 total_residence_time = tau3_c + tau3_d
 
